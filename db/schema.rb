c1e0852b2d500d0a71a008df5a2f87b06923a566
Laureen Burd <laureen.burd@gmail.com>
2018-04-27 19:42:15 -0400

    4.2 added color, year and price fields, added products, migrated database and styled product page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427003957) do
+ActiveRecord::Schema.define(version: 20180427010156) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180427003957) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.integer "year"
+    t.integer "price"
   end
 
 end
