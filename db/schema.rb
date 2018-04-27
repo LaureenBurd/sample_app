44a3aca4e4f56d8847cb52db148f18251e38a84e
Laureen Burd <laureen.burd@gmail.com>
2018-04-26 20:41:37 -0400

    4.2 work with tutor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423195650) do
+ActiveRecord::Schema.define(version: 20180427003957) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20180423195650) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.integer "year"
   end
 
 end
