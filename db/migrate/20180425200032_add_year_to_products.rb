c5babc2fd993b0e01038b843f412973678d9e4da
Laureen Burd <laureen.burd@gmail.com>
2018-05-06 17:57:00 -0400

    4.4 fixing duplicate migration error

--- db/migrate/20180425200032_add_year_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddYearToProducts < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :products, :brand, :string
-    add_column :products, :year, :number
-    add_column :products, :price, :number
-  end
-end
