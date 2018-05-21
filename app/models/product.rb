cce928e9ea7b72a1d5d043c7a2a8b7ba4cef149a
Laureen Burd <laureen.burd@gmail.com>
2018-05-21 13:20:54 -0400

    5.2 created a searchform

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ApplicationRecord
 	has_many :orders
 end
+
+def self.search(search_term)
+  Product.where("name ilike ?", "%#{search_term}%")
+end
+
+def index
+  if params[:q]
+    search_term = params[:q]
+    @products = Product.search(search_term)
+  else
+    @products = Product.all
+  end
+end
