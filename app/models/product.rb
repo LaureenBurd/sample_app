28f6f25267182d6fac84dcf6203f844bed62cd2d
Laureen Burd <laureen.burd@gmail.com>
2018-05-22 14:42:15 -0400

    5.2 corrected product.rb to fix search button not working on heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,11 @@ class Product < ApplicationRecord
 end
 
 def self.search(search_term)
-  Product.where("name ilike ?", "%#{search_term}%")
+  if Rails.env.production?
+		Product.where("name ilike?", "%#{search_term}%")
+	else
+		Product.where("name LIKE ?", "%#{search_term}%")
+	end
 end
 
 def index
