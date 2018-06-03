1972ae8436dfd42d2ff4ce8079507413c616d9cb
Laureen Burd <laureen.burd@gmail.com>
2018-06-03 12:51:43 -0400

    5.7 created comment model, set up comment model associations, set up scopes for comment model and custom methods for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :orders
+  has_many :comments
 end
 
 def self.search(search_term)
@@ -17,4 +18,8 @@ def index
   else
     @products = Product.all
   end
+end
+
+def highest_rating_comment
+  comments.rating_desc.first
 end
