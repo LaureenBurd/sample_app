9791be5b855d05a6cae0215e626a4ab124999c7e
Laureen Burd <laureen.burd@gmail.com>
2018-06-03 16:15:19 -0400

    5.7 updated product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,25 +1,18 @@
 class Product < ApplicationRecord
 	has_many :orders
   has_many :comments
-end
 
-def self.search(search_term)
-  if Rails.env.production?
-		Product.where("name ilike?", "%#{search_term}%")
-	else
-		Product.where("name LIKE ?", "%#{search_term}%")
-	end
-end
 
-def index
-  if params[:q]
-    search_term = params[:q]
-    @products = Product.search(search_term)
-  else
-    @products = Product.all
+  def self.search(search_term)
+    if Rails.env.production?
+  		Product.where("name ilike?", "%#{search_term}%")
+  	else
+  		Product.where("name LIKE ?", "%#{search_term}%")
+  	end
   end
-end
 
-def highest_rating_comment
-  comments.rating_desc.first
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
 end
