20f9ccbc83ab914b95c0beb11230839272a5e193
Laureen Burd <laureen.burd@gmail.com>
2018-06-12 22:03:18 -0400

    5.9 implemented will_paginate gem on product reviews and added validation to all fields in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ApplicationRecord
 	has_many :orders
   has_many :comments
   validates :name, presence: true
+  validates :style, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :speed, presence: true
+  validates :gender, presence: true
+  validates :price, presence: true
+  validates :image_url, presence: true
 
 
   def self.search(search_term)
