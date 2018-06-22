46baa01000697302c1e4b01bff9e11e8364d48f3
Laureen Burd <laureen.burd@gmail.com>
2018-06-22 12:31:13 -0400

    5.11 updates with mentor

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 	default from: "customerservice@bikeberlin.com"
 
 	def contact_form(email, name, message)
-  @message = message
+  	@message = message
     mail(from: email,
          to: 'laureen.burd@gmail.com',
          subject: "A new contact form message from #{name}")
@@ -14,13 +14,5 @@ class UserMailer < ApplicationMailer
 	  mail(to: user.email,
 	       subject: "Welcome to #{@appname}!")
 	end
-
-
-	def thank_you
-	    @name = params [:name]
-	    @email = params [:email]
-	    @message = params [:message]
-	    UserMailer.contact_form(@email, @name, @message).deliver_now
-	  end  
   
 end
